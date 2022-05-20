f14c1d0519456fbe5f6a78ff22684954b16f96c4
mMemeMan <artur.gubanovich@gmail.com>
2022-05-20 22:47:42 +0200

    the code has been slightly improved

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,11 +7,11 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        Music music = context.getBean("musicBean", Music.class);
-
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+//        Dependency Injection
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
